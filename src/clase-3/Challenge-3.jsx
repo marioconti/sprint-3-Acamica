86fb28cf401021efbaaab50e8f4ed10fbe8c197f
Mario Conti <marioConti91@gmail.com>
2021-09-01 11:58:09 -0300

    se agrega las cards al challenge y lógica del renderizado, falta arreglar bugs

--- src/clase-3/Challenge-3.jsx
+++ src/clase-3/Challenge-3.jsx
@@ -1,6 +1,25 @@
 import React from "react";
 import "./estilos-3.css";
 
+const arrayImagenes = [
+  {
+    imagen: "./images/hoja1.jpg",
+  },
+  {
+    imagen: "./images/hoja2.jpg",
+  },
+  {
+    imagen: "./images/hoja3.jpg",
+  },
+  {
+    imagen: "./images/hoja4.jpg",
+  },
+  {
+    imagen: "./images/hoja5.jpg",
+  },
+];
+
+//==================================================
 export const ChallengeClaseTres = () => {
   return (
     <>
@@ -11,6 +30,9 @@ export const ChallengeClaseTres = () => {
     </>
   );
 };
+
+//==================================================
+// HEADER
 const Header = () => {
   return (
     <div className="header">
@@ -19,7 +41,8 @@ const Header = () => {
     </div>
   );
 };
-
+//==================================================
+// CUERPO
 const Cuerpo = () => {
   return (
     <>
@@ -30,21 +53,49 @@ const Cuerpo = () => {
     </>
   );
 };
-
+//==================================================
+// SECCION/RESULTADOS
 const Seccion = () => {
   return (
     <div className="seccion">
-      Seccion
-      <div>Sub titulo</div>
+      <h1>Secciónn</h1>
+      <h2>Subtitulo</h2>
+      <div className="seccion-galeria">
+        <Card imgSrc={arrayImagenes[0].imagen} />
+        <Card imgSrc={arrayImagenes[1].imagen} />
+        <Card imgSrc={arrayImagenes[2].imagen} />
+        <Card imgSrc={arrayImagenes} />
+        <Card imgSrc={arrayImagenes[4].imagen} />
+      </div>
     </div>
   );
 };
-
+//==================================================
+// FOOTER
 const Footer = () => {
   return (
     <div className="footer">
       Footer
       <div>Sub titulo</div>
+    </div>
+  );
+};
+//==================================================
+// CARD
+// FIXME: arreglar la logica para q no muestre nada cuando no está la imagen o cuando no se encuentra la imagen
+const Card = (props) => {
+  return (
+    <div className="contenedor-Card">
+      {/* sin mensaje de error */}
+      {/* {props.imgSrc && (
+        <img className="imagen" src={props.imgSrc} alt="imagen renderizada" />
+      )} */}
+
+      {!props.imgSrc ? (
+        <h2>No se encontró la imagen</h2>
+      ) : (
+        <img className="imagen" src={props.imgSrc} alt="imagen renderizada" />
+      )}
     </div>
   );
 };
